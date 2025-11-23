c112d0c36cf44dbb1d24f3995ca9343b0fcf7c2e
BaeJongOk <0511jongok@gmail.com>
2025-11-23 16:53:27 +0900

    - 유레카 서버 보여지는게 예쁘지 않아 수정중입니다.

--- src/main/java/com/nhnacademy/_vidiadiscovery/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/_vidiadiscovery/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,6 +39,11 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/eureka/css/**", "/eureka/js/**");
     }
 }
 
